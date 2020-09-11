5857e876df78cb404463f7bb188900a4b2910d11
KirstenBirchHaakansson <kih@aqua.dtu.dk>
2020-09-11 20:41:30 +0200

    Running all hie

--- WKRDB-EST2/testData/CreateTestData.R
+++ WKRDB-EST2/testData/CreateTestData.R
@@ -50,7 +50,7 @@ allRequiredTables <- getTablesInHierarchies(downloadFromGitHub = FALSE, fileLoca
 ## STEP 2) GENERATE TEST DATA
 
 # Can use a loop to generate test data for all hierarchies if you want to 
-for (i in 1:1){
+for (i in 1:13){
   myHierarchyToGenerate <- paste('H',i,sep="")
   
   # Define some parameters for our test data
