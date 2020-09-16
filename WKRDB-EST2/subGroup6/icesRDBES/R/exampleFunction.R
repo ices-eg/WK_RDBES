c0aa0aa1e8a4826665cc8d2bfbcb4aaf56576f61
Richard Meitern <rix133@ut.ee>
2020-09-16 15:10:12 +0300

    rename file

--- WKRDB-EST2/subGroup6/icesRDBES/R/exampleFunction.R
+++ WKRDB-EST2/subGroup6/icesRDBES/R/exampleFunction.R
@@ -1,5 +1,8 @@
 #' Minimum Documentation Example
 #'
+#' See the file *"./tests/testthat/test-exampleFunction.R"*
+#' for defining expected input and output
+#'
 #' @param stringX first string
 #' @param stringY second string
 #'
