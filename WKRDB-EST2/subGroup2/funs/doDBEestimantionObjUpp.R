75d11e6d82bfe07d7113b8391ceb5df7f5d39dfa
KirstenBirchHaakansson <kih@aqua.dtu.dk>
2020-09-16 20:36:30 +0200

    Updated the documentation a bit

--- WKRDB-EST2/subGroup2/funs/doDBEestimantionObjUpp.R
+++ WKRDB-EST2/subGroup2/funs/doDBEestimantionObjUpp.R
@@ -1,15 +1,15 @@
-#' generic_su_object_upper_hie
+#' Generates the DBE estimation object for the upper hierarchy tables
 #'
 #' @param input_list All the data tables in a named list. Name should be equal 
 #' to the short table names e.g. DE, SD, TE, FO. An example can be found at the share point: 
 #' https://community.ices.dk/ExpertGroups/WKRDB/2019%20Meetings/WKRDB-EST%202019/06.%20Data/Kirsten/H1/H1_upper.RData
-#' @param hierarchy The number of the hierarchy you are inputting - 1 to 13
 #' 
 #'
-#' @return
+#' @return The upper hierarchy tables in the DBE estimation object (DBEestimantionObjUpp)
 #' @export
 #'
 #' @examples
+#' 
 #' 
 
 
