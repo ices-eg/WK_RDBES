8c5040f0cfc948c439e93bc34b9920e4871efce1
KirstenBirchHaakansson <kih@aqua.dtu.dk>
2020-09-16 11:37:01 +0200

    Moving old stuff to a old folder

--- WKRDB-EST2/subGroup2/funs/old_scripts/call_funs.R
+++ WKRDB-EST2/subGroup2/funs/old_scripts/call_funs.R
@@ -5,6 +5,31 @@ load("Q:/dfad/users/kibi/data/WK_RDBES/Kirsten/H1/H1_upper.RDATA")
 
 # Add inclusion probabilities if missing
 
+H1_upper$VS$VSprob <- 1-(1-1/H1_upper$VS$VStotal)^H1_upper$VS$VSsampled
+
+H1_upper$FT$FTprob <- 1-(1-1/H1_upper$FT$FTtotal)^H1_upper$FT$FTsampled
+
+H1_upper$FO$FOprob <- 1-(1-1/H1_upper$FO$FOtotal)^H1_upper$FO$FOsampled
+
+source("./WKRDB-EST2/subGroup2/funs/fun_generic_su_object_upper_hie.R")
+
+test_old <-
+  generic_su_object_upper_hie(input_list = H1_upper, hierachy = 1)
+
+source("./WKRDB-EST2/subGroup2/personal/Kirsten/fun_generic_su_object_upper_hie.R")
+
+test_new <-
+  generic_su_object_upper_hie(input_list = H1_upper, hierachy = 1)
+# I want to estimate total number of vessels
+
+
+
+total_vessel_strata <-
+  data.frame(
+    stratum = test$su1$designTable$stratum,
+    probs = test$su1$comb_select_probs
+  )
+
 
 
 
@@ -22,6 +47,7 @@ test <-
 # test_prob_1 <-
 #   fun_add_probs(input_list = test_prob, type_of_probs = "selection")
 
+test_prob <- function(input_list = test, type_of_probs = "inclusion")
 
 total_vessel_strata <-
   data.frame(
