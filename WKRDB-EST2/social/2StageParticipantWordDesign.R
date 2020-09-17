28a512996db21ffc7cbcbf10120f26e6530379b3
nmprista <31890653+nmprista@users.noreply.github.com>
2020-09-17 19:26:34 +0200

    Update 2StageParticipantWordDesign.R
    bug fixed

--- WKRDB-EST2/social/2StageParticipantWordDesign.R
+++ WKRDB-EST2/social/2StageParticipantWordDesign.R
@@ -5,12 +5,10 @@ colnames(PSU)<-"Participant"
 SSU$Word<-as.character(SSU$Word)
 PSU$Participant<-as.character(PSU$Participant)
 
-PSU<-PSU[PSU$Participant %in% c("Johnathan Ball","David Currie","Kirsten Birch HÃ¥kansson","Edvin Fuglebakk","Nuno Prista","Marta Suska"),]
-
 for (i in 1:2)
 {
 print("===")
-print(sample(PSU, size=1, replace=FALSE))
+print(sample(PSU$Participant, size=1, replace=FALSE))
 print("========")
 print(sample(SSU$Word, size = 2, replace=FALSE))
 }
