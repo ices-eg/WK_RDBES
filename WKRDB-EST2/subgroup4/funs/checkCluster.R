e0389b8fcd69605724658e76311a537ddb3c73d8
Ana Fernandes <49688265+anacfernandes@users.noreply.github.com>
2021-09-23 12:29:10 +0100

    Update checkCluster.R

--- WKRDB-EST2/subgroup4/funs/checkCluster.R
+++ WKRDB-EST2/subgroup4/funs/checkCluster.R
@@ -1,6 +1,6 @@
 checkCluster <- function(x=x3){
 # Ana Fernandes
-# makes a check for presence of stratification in SS
+# makes a check for presence of clustering in SS
 for (i in 1:length(x$SSid)){
 if (length(b<-x[x$SSclustering!='N',]$SSid)>0){
 	print ("ATTENTION: there is clustering for SSid's:")
