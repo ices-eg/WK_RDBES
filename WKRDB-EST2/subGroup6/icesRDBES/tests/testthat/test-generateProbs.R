92fbffa63fc794c40bc832358045532658f88155
David Currie <davidcurrie2001@yahoo.co.uk>
2021-09-23 17:48:09 +0100

    Delete redundant tests

--- WKRDB-EST2/subGroup6/icesRDBES/tests/testthat/test-generateProbs.R
+++
@@ -1,9 +0,0 @@
-test_that("Test generateProbs fails for incorrect input", {
-
-  # We might want to be more specific about the error messages we test for here
-  expect_error(generateProbs(x = "banana", probType = "selection"))
-  expect_error(generateProbs(x = list("a" = "silly"), probType = "selection"))
-  expect_error(generateProbs(x = NA, probType = "selection"))
-  expect_error(generateProbs(x = NULL, probType = "selection"))
-  expect_error(generateProbs(x = "banana", probType = "anotherMethod"))
-})
