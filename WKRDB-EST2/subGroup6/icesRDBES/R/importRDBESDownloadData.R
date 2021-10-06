e129ba9094787ff1d9f2dea8ed817f4c1b8be083
Richard Meitern <rix133@ut.ee>
2021-10-06 15:51:35 +0300

    fix style

--- WKRDB-EST2/subGroup6/icesRDBES/R/importRDBESDownloadData.R
+++ WKRDB-EST2/subGroup6/icesRDBES/R/importRDBESDownloadData.R
@@ -15,43 +15,50 @@
 #' @examples
 #' files <- c("./tests/testthat/h1_v_1_19/H1_2021_000_example.zip")
 #' obj <- importRDBESDownloadData(files)
-importRDBESDownloadData <- function(filenames){
+importRDBESDownloadData <- function(filenames) {
   randInt <- paste0(sample(1:100, 3), collapse = "")
   tmp <- paste0(tempdir(), "/downloadImport", randInt)
   dir.create(tmp)
 
-  unzipFile <- function(x,tmp){
-    if(!file.exists(x)) {return()}
-    if(is.zip(x)){return(utils::unzip(x, exdir = tmp))}
-    if(fileExt(x) == "csv"){
+  unzipFile <- function(x, tmp) {
+    if (!file.exists(x)) {
+      return()
+    }
+    if (is.zip(x)) {
+      return(utils::unzip(x, exdir = tmp))
+    }
+    if (fileExt(x) == "csv") {
       newName <- paste0(tmp, "/", basename(x))
-      if(file.exists(newName)){
-        warning(paste0("Overwriting file: ", basename(x),
-                      ", this might be intended!\n"), call. = FALSE)
+      if (file.exists(newName)) {
+        warning(paste0(
+          "Overwriting file: ", basename(x),
+          ", this might be intended!\n"
+        ), call. = FALSE)
       }
       file.copy(x, tmp)
       return(newName)
     }
-
   }
 
-  fileExt <- function(x){
+  fileExt <- function(x) {
     ext <- strsplit(x, ".", fixed = TRUE)[[1]]
     ext[length(ext)]
   }
 
-  is.zip <- function(x){
-    #let's assume that the file extension is .zip to be on the safe side
+  is.zip <- function(x) {
+    # let's assume that the file extension is .zip to be on the safe side
     ext <- fileExt(x)
-    #the zip starts with a local file header signature 50 4b 03 04
-    #see: https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT
-    b <- readBin(x, "raw",4)
-    if(setequal(as.character(b), c("50", "4b", "03", "04")) &
-       ext == "zip") return(TRUE)
+    # the zip starts with a local file header signature 50 4b 03 04
+    # see: https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT
+    b <- readBin(x, "raw", 4)
+    if (setequal(as.character(b), c("50", "4b", "03", "04")) &
+      ext == "zip") {
+      return(TRUE)
+    }
     FALSE
   }
 
-  #the files are not used currently but can be if we want to
+  # the files are not used currently but can be if we want to
   files <- unique(unlist(sapply(filenames, unzipFile, tmp)))
   res <- createRDBESRawObject(tmp)
   unlink(tmp, recursive = T)
--- WKRDB-EST2/subGroup6/icesRDBES/tests/testthat/test-importRDBESDownloadData.R
+++ WKRDB-EST2/subGroup6/icesRDBES/tests/testthat/test-importRDBESDownloadData.R
@@ -1,29 +1,35 @@
 # common parameters
 ddir <- "./h1_v_1_19/"
-expObj  <- readRDS(paste0(ddir, "h1_rdbesRawObject.rds"))
+expObj <- readRDS(paste0(ddir, "h1_rdbesRawObject.rds"))
 
 
 test_that("importing zipped H1 example data works", {
-  zipFiles <- c("H1_2021_000_example.zip",
-             "HSL_2021_002_example.zip",
-             "HVD_2021_001_example.zip")
+  zipFiles <- c(
+    "H1_2021_000_example.zip",
+    "HSL_2021_002_example.zip",
+    "HVD_2021_001_example.zip"
+  )
 
   genObj <- importRDBESDownloadData(paste0(ddir, zipFiles))
   expect_equal(genObj, expObj)
 })
 
 test_that("importing some data that is not zipped H1 example data works", {
-  zipFiles <- c("H1_2021_000_example.zip",
-                "HSL_2021_002_example.zip",
-                "VesselDetails.csv")
+  zipFiles <- c(
+    "H1_2021_000_example.zip",
+    "HSL_2021_002_example.zip",
+    "VesselDetails.csv"
+  )
 
   genObj <- importRDBESDownloadData(paste0(ddir, zipFiles))
   expect_equal(genObj, expObj)
 })
 
 test_that("importing subset H1 example data works", {
-  zipFiles <- c("HSL_2021_002_example.zip",
-                "VesselDetails.csv")
+  zipFiles <- c(
+    "HSL_2021_002_example.zip",
+    "VesselDetails.csv"
+  )
 
   genObj <- importRDBESDownloadData(paste0(ddir, zipFiles))
   expect_equal(genObj$VD, expObj$VD)
@@ -32,9 +38,13 @@ test_that("importing subset H1 example data works", {
 })
 
 test_that("Overwriting a tabel from a zip file produces a warning", {
-  zipFiles <- c("HVD_2021_001_example.zip",
-                "VesselDetails.csv")
+  zipFiles <- c(
+    "HVD_2021_001_example.zip",
+    "VesselDetails.csv"
+  )
 
-  expect_warning(importRDBESDownloadData(paste0(ddir, zipFiles)),
-                 "Overwriting file: VesselDetails.csv, this might be intended!")
+  expect_warning(
+    importRDBESDownloadData(paste0(ddir, zipFiles)),
+    "Overwriting file: VesselDetails.csv, this might be intended!"
+  )
 })
