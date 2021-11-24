8d28d88e8db5c13248f6236e819d1a6caa1368f1
nmprista <31890653+nmprista@users.noreply.github.com>
2021-11-24 17:38:21 +0100

    update after styler and lintr

--- WKRDB-EST2/chairs/Nuno/R/generateProbs.r
+++ WKRDB-EST2/chairs/Nuno/R/generateProbs.r
@@ -1,104 +1,101 @@
-#' Generate vector of selection or inclusion probabilities
-#' (version of function by John Ball)
+#' Generate selection and inclusion probabilities
+#' (worked from version of function by John Ball)
 #'
 #' @param x RDBES data frame
-#' @param probType  Value == "selection" | "inclusion" ,
-# for selection or inclusion probabilities respectively
+#' @param probType  Value == "selection" | "inclusion" | "both" ,
+#' for selection and/or inclusion probabilities respectively
+#' @param overwrite  Value == TRUE ,
+#' for selection and/or inclusion probabilities respectively
 #'
 #' @return A vector or probabilities
 #' @export
 #'
-#' @examples
-#' \dontrun{
-#' generateProbs(x=y, probType="inclusion")
-#' }
+#' examples for now see
+#' https://github.com/ices-eg/WK_RDBES/tree/master/WKRDB-EST2/chairs/Nuno
+#'
 
 
 generateProbs <- function(x, probType, overwrite) {
   x <- data.frame(x)
 
   a <- x[grepl("selectMeth", names(x)) == T]
-  a <- as.character(a[,grepl("selectMethCluster", names(a)) == F])
+  a <- as.character(a[, grepl("selectMethCluster", names(a)) == F])
 
   if (sum(is.na(a)) > 0) stop("cannot proceed: NAs in SSselectMeth")
 
-  if (length(unique(a[a!="OutOfFrame"])) > 1) {
-		print ("warning: two different selection methods")
-		if(any(c("FIXED","Unknown") %in% a)) {}
-}
+  if (length(unique(a)) > 1) {
+    print("warning: two different selection methods")
+  }
 
-		# set in line with guideline RDBES issue #71
-	
+  # set in line with guideline RDBES issue #71
+
 
 
   vecSmallN <- x[grepl("numSamp", names(x)) == T]
-  vecSmallN <- vecSmallN[,grepl("SampCluster", names(vecSmallN)) == F]
+  vecSmallN <- vecSmallN[, grepl("SampCluster", names(vecSmallN)) == F]
   vecBigN <- x[grepl("numTotal", names(x)) == T]
-  vecBigN <- vecBigN[,grepl("TotalCluster", names(vecBigN)) == F]
+  vecBigN <- vecBigN[, grepl("TotalCluster", names(vecBigN)) == F]
 
-		if(overwrite==T) { # can only overwrite if there is info on vecBigN
-		if (sum(is.na(vecBigN)) > 0) stop("cannot proceed: NAs in numTotal")
-		if (sum(is.na(vecSmallN)) > 0) {
-			if(!x$recType[1]=="BV"){
-				print("missing numSamp: generating numSamp from row number")
-				x$numSamp <- vecSmallN <- rep(length(vecSmallN),length(vecSmallN))
-				} else {
-					print("missing numSamp: generating numSamp from number of unique fishId")
-					x$numSamp <- vecSmallN <- rep(length(unique(x$fishId)),length(vecSmallN))
-					}
-		}
-		}
+  if (overwrite == T) { # can only overwrite if there is info on vecBigN
+    if (sum(is.na(vecBigN)) > 0) stop("cannot proceed: NAs in numTotal")
+    if (sum(is.na(vecSmallN)) > 0) {
+      if (!x$recType[1] == "BV") {
+        print("missing numSamp: generating numSamp from row number")
+        x$numSamp <- vecSmallN <- rep(length(vecSmallN), length(vecSmallN))
+      } else {
+        print("missing numSamp: generating numSamp from number of
+              unique fishId")
+        x$numSamp <- vecSmallN <- rep(length(unique(x$fishId)),
+                                      length(vecSmallN))
+      }
+    }
+  }
 
-  if (probType %in% c("selection","both")) {
+  if (probType %in% c("selection", "both")) {
+    vecProb <- x[grepl("selProb", names(x)) == T] # not defined
+    if (!x$recType[1] %in% c("SA")) vecProb <-
+        vecProb[, grepl("selProbCluster", names(vecProb)) == F]
 
-	vecProb <- x[grepl("selProb", names(x)) == T] # not defined
-	if(!x$recType[1] %in% c("SA")) vecProb <- vecProb[,grepl("selProbCluster", names(vecProb)) == F] 
-
-	if (a[1] %in% c("SRSWR", "SRSWOR")) {
-      if(overwrite==T) {
-	  if (a[1] == "SRSWR") vecProb <- 1 / vecBigN
-      if (a[1] == "SRSWOR") {
-		print ("warning: prob selection depends on order...assuming samples taken sequentially in order of rows")
-		vecProb <- 1/(vecBigN[1]:(vecBigN[1]- vecSmallN[1]+1))
-		}
-		}
+    if (a[1] %in% c("SRSWR", "SRSWOR")) {
+      if (overwrite == T) {
+        if (a[1] == "SRSWR") vecProb <- 1 / vecBigN
+        if (a[1] == "SRSWOR") {
+          print("warning: prob selection depends on order...assuming
+                samples taken sequentially in order of rows")
+          vecProb <- 1 / (vecBigN[1]:(vecBigN[1] - vecSmallN[1] + 1))
+        }
+      }
     }
     if (a[1] %in% c("UPSWR", "UPSWOR")) {
-      if (sum(is.na(vecProb)) > 0) stop ("cannot proceed: NAs in sampProb")
+      if (sum(is.na(vecProb)) > 0) stop("cannot proceed: NAs in sampProb")
       vecProb <- vecProb
-	  if(overwrite==T) { stop ("cannot overwrite probSelection in UP designs") }
+      if (overwrite == T) {
+        stop("cannot overwrite probSelection in UP designs")
+      }
     }
-    x$selProb<-vecProb  
+    x$selProb <- vecProb
   }
 
 
   if (probType %in% c("inclusion", "both")) {
-	
-	vecProb <- x[grepl("incProb", names(x)) == T]
-	if(x$recType[1]!="SA") vecProb <- vecProb[,grepl("incProbCluster", names(vecProb)) == F]
+    vecProb <- x[grepl("incProb", names(x)) == T]
+    if (x$recType[1] != "SA") vecProb <-
+        vecProb[, grepl("incProbCluster", names(vecProb)) == F]
     if (a[1] %in% c("SRSWR", "SRSWOR", "CENSUS")) {
-		if(overwrite==T) {
-		if (a[1] == "SRSWR") vecProb <- 1 - (1 - 1 / vecBigN)^vecSmallN
-		if (a[1] == "SRSWOR") vecProb <- vecSmallN / vecBigN
-		if (a[1] == "CENSUS") vecProb <- rep(1,length(vecBigN))
-		}
-	  }
-    if (a[1] %in% c("UPSWR", "UPSWOR")) {
-        if (sum(is.na(vecProb)) > 0) stop("cannot proceed: NAs in sampProb")
-        vecProb <- vecProb
-		if(overwrite==T) { stop ("cannot overwrite probInclusion in UP designs") }
+      if (overwrite == T) {
+        if (a[1] == "SRSWR") vecProb <- 1 - (1 - 1 / vecBigN)^vecSmallN
+        if (a[1] == "SRSWOR") vecProb <- vecSmallN / vecBigN
+        if (a[1] == "CENSUS") vecProb <- rep(1, length(vecBigN))
       }
-   x$incProb<-vecProb 
+    }
+    if (a[1] %in% c("UPSWR", "UPSWOR")) {
+      if (sum(is.na(vecProb)) > 0) stop("cannot proceed: NAs in sampProb")
+      vecProb <- vecProb
+      if (overwrite == T) {
+        stop("cannot overwrite probInclusion in UP designs")
+      }
+    }
+    x$incProb <- vecProb
   }
   x
 }
-
-
-
-# did not handle Quasi - user should edit it.
-# treat.Q
-
-# possible checks
-# n>N in "SRSWOR" or "CENSUS" or other
-
-# contains "OutOfFrame" or not
